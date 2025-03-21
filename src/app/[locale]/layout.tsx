677743ac8c24c99fb97c74b85af57cda25fe10ee
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-03-21 16:40:00 +0200

    Add internationalization support with Arabic and English translations

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,47 @@
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { notFound } from "next/navigation";
+import { getTranslations, setRequestLocale } from "next-intl/server";
+
+import { routing } from "@/i18n/routing";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string };
+}) {
+  const { locale } = params;
+
+  // Fetch translations for metadata
+  const t = await getTranslations({ locale, namespace: "Metadata" });
+
+  return {
+    title: t("title"), // Set the page title
+  };
+}
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  setRequestLocale(locale);
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
